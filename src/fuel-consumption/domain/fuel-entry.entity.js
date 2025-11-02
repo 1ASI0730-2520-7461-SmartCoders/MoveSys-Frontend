025a64b1e32399b9f5906ed3f63f4ebf45b86562
Gabriel <148838535+zzZero14@users.noreply.github.com>
2025-11-02 02:08:19 -0500

    refactor: fuel entry entity for cost calculations

--- src/fuel-consumption/domain/fuel-entry.entity.js
+++ src/fuel-consumption/domain/fuel-entry.entity.js
@@ -3,10 +3,12 @@ export class FuelEntry {
     id = null,
     vehicleId = null,
     vehiclePlate = '',
+    model = '',
     liters = 0,
-    cost = 0,
+    costPerLiter = 0,
+    totalPaid = 0,
     fuelType = 'diesel',
-    station = '',
+    provider = '',
     filledAt = null,
     odometer = null,
     notes = ''
@@ -14,19 +16,22 @@ export class FuelEntry {
     this.id = id;
     this.vehicleId = vehicleId;
     this.vehiclePlate = vehiclePlate;
+    this.model = model;
     this.liters = liters;
-    this.cost = cost;
+    this.costPerLiter = costPerLiter;
+    this.totalPaid = totalPaid;
     this.fuelType = fuelType;
-    this.station = station;
+    this.provider = provider;
     this.filledAt = filledAt;
     this.odometer = odometer;
     this.notes = notes;
   }
 
-  get costPerLiter() {
-    if (!this.liters) return 0;
-    return this.cost / this.liters;
+  get cost() {
+    return this.totalPaid || (this.liters * this.costPerLiter);
+  }
+
+  get calculatedTotal() {
+    return this.liters * this.costPerLiter;
   }
 }
-
-
